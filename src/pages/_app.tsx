b2baeeca676fd5ccab8b872d056222cdd3d52cf1
Chopper <riskidrian1@gmail.com>
2023-09-18 15:41:31 +0700

    add Package nextjs-progressbar and use in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,12 @@ import PageLayout from "@/components/layouts/PageLayout";
 import { GlobalContextProvider } from "@/contexts/globalContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
 import { Heebo } from "next/font/google";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+
+import NextNProgress from "nextjs-progressbar";
 
 // If loading a variable font, you don't need to specify the font weight
 const heebo = Heebo({ subsets: ["latin"] });
@@ -20,6 +21,13 @@ export default function App({ Component, pageProps }: AppProps) {
     <GlobalContextProvider>
       <main className={heebo.className}>
         <PageLayout>
+          <NextNProgress
+            color="#6512B9"
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={5}
+            showOnShallow={true}
+          />
           <Component {...pageProps} />
         </PageLayout>
       </main>
