b5d7df56c90eee626c5e8147027727076709b1ef
Chopper <riskidrian1@gmail.com>
2024-12-27 20:54:27 +0700

    chore: age

--- src/views/home/sections/MeSection.tsx
+++ src/views/home/sections/MeSection.tsx
@@ -4,8 +4,8 @@ const MeSection = () => {
   return (
     <section
       data-aos="fade-up"
-      className="bg-bg-partial border border-slate-me rounded-md p-3 flex flex-col justify-center md:p-8 lg:mx-5 lg:border-none">
-      <div className="border-b w-full border-slate-me pb-3 my-3 md:pb-5 lg:flex lg:items-center lg:justify-between ">
+      className="flex flex-col justify-center p-3 border rounded-md bg-bg-partial border-slate-me md:p-8 lg:mx-5 lg:border-none">
+      <div className="w-full pb-3 my-3 border-b border-slate-me md:pb-5 lg:flex lg:items-center lg:justify-between ">
         <div className="flex flex-col items-center md:items-start md:flex-row md:gap-4">
           <Image
             className="rounded-full lg:hidden"
@@ -15,7 +15,7 @@ const MeSection = () => {
             alt="me"
           />
           <div className="text-center md:text-start">
-            <h4 className="text-xl text-text-color font-medium mt-4 xl:text-2xl">
+            <h4 className="mt-4 text-xl font-medium text-text-color xl:text-2xl">
               Front-end Developer
             </h4>
             <h1 className="text-2xl font-bold xl:text-4xl">
@@ -23,15 +23,15 @@ const MeSection = () => {
             </h1>
           </div>
         </div>
-        <div className="flex justify-evenly mt-4 md:justify-start md:gap-6">
+        <div className="flex mt-4 justify-evenly md:justify-start md:gap-6">
           <a
-            className="rounded-md border border-slate-me transition-all duration-300 ease-in-out hover:opacity-80 hover:scale-125"
+            className="transition-all duration-300 ease-in-out border rounded-md border-slate-me hover:opacity-80 hover:scale-125"
             href="https://www.instagram.com/driannaird/"
             target="_blank">
             <Image src="/icons/instagram.svg" width="40" height="40" alt="ig" />
           </a>
           <a
-            className="rounded-md border border-slate-me transition-all duration-300 ease-in-out hover:opacity-80 hover:scale-110"
+            className="transition-all duration-300 ease-in-out border rounded-md border-slate-me hover:opacity-80 hover:scale-110"
             href="https://www.youtube.com/@driannaird"
             target="_blank">
             <Image
@@ -42,7 +42,7 @@ const MeSection = () => {
             />
           </a>
           <a
-            className="rounded-md border border-slate-me transition-all duration-300 ease-in-out hover:opacity-80 hover:scale-125"
+            className="transition-all duration-300 ease-in-out border rounded-md border-slate-me hover:opacity-80 hover:scale-125"
             href="https://www.tiktok.com/@driannaird"
             target="_blank">
             <Image
@@ -53,7 +53,7 @@ const MeSection = () => {
             />
           </a>
           <a
-            className="rounded-md border border-slate-me transition-all duration-300 ease-in-out hover:scale-125 hover:opacity-80"
+            className="transition-all duration-300 ease-in-out border rounded-md border-slate-me hover:scale-125 hover:opacity-80"
             href="https://github.com/Drian-key"
             target="_blank">
             <Image
@@ -65,10 +65,10 @@ const MeSection = () => {
           </a>
         </div>
       </div>
-      <div className="text-center text-base border-b w-full border-slate-me pb-3 mb-3 md:text-lg md:text-start">
+      <div className="w-full pb-3 mb-3 text-base text-center border-b border-slate-me md:text-lg md:text-start">
         <div className="mb-3">
-          <span className="bg-gradient-to-r from-purple-500 to-pink-is text-transparent bg-clip-text font-bold lg:text-xl">
-            #front-end #19yearsold
+          <span className="font-bold text-transparent bg-gradient-to-r from-purple-500 to-pink-is bg-clip-text lg:text-xl">
+            #front-end #20yearsold
           </span>
         </div>
         <h3 className="text-gray-300 md:text-lg">
@@ -77,7 +77,7 @@ const MeSection = () => {
         </h3>
       </div>
       <div>
-        <button className="bg-gradient-to-r from-unggu-is to-pink-is w-full py-2 rounded-md border border-slate-me flex justify-center items-center gap-1 text-base transition-all duration-300 ease-in-out hover:scale-105 hover:opacity-80 md:w-auto md:px-4 md:text-lg">
+        <button className="flex items-center justify-center w-full gap-1 py-2 text-base transition-all duration-300 ease-in-out border rounded-md bg-gradient-to-r from-unggu-is to-pink-is border-slate-me hover:scale-105 hover:opacity-80 md:w-auto md:px-4 md:text-lg">
           <Image src="/icons/unduh.svg" width={25} height={25} alt="unduh" />
           Download CV (Soon)
         </button>
